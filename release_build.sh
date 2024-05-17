8fe3f8e1a355261e187a05d2a31590a7219f268e
christer johansson <christerjoha@gmail.com>
2024-05-17 14:35:26 +0200

    update build script

--- release_build.sh
+++ release_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build --rm -t aws-slack-notification .
 mkdir -p release_build
-docker run --platform linux/arm64 -v $PWD/release_build:/release_build  -t  aws-slack-notification
+docker run --rm --platform linux/arm64 -v $PWD/release_build:/release_build  -t  aws-slack-notification
